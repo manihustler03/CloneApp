7ec64f27534b3b7356a83a68da8745a316f5c159
arun <arunkannansm@gmail.com>
2024-08-01 21:37:49 +0530

    update: Arun Page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
   return (
     <>
+      <Example />
       <h1 className="text-3xl font-bold  bg-gradient-to-r from-red-600  to-yellow-400 inline-block text-transparent bg-clip-text">
       Instagram Clone
     </h1>
