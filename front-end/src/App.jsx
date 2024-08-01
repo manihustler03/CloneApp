08553e3a498ff750066fe8bf246dd935058f9cc9
Sundara Manikandan <manihustler03@gmail.com>
2024-08-01 20:54:27 +0530

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,8 +4,8 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <h1 className="text-3xl font-bold  bg-gradient-to-r from-red-600  to-yellow-400 inline-block text-transparent bg-clip-text">
+      Instagram Clone
     </h1>
     </>
   )
