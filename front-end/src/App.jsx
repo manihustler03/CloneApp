cdcd22d0b310c18f18fd2685085fbe226ed8995b
Sundara Manikandan <manihustler03@gmail.com>
2024-08-01 21:53:41 +0530

    App.jsx Modified

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-import './App.css'
+
 import Button from './Button'
+import ArunPage from './ArunPage'
 
 function App() {
   return (
     <>
-      <Example />
+    <ArunPage/>
       <h1 className="text-3xl font-bold  bg-gradient-to-r from-red-600  to-yellow-400 inline-block text-transparent bg-clip-text">
       Instagram Clone
     </h1>
     <p>This app is fully responsive</p>
     <Button/>
+    
     </>
   )
 }
