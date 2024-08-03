9a095137e181a851967d8ed0ac5f516c33a3cfb4
Sundara Manikandan <manihustler03@gmail.com>
2024-08-03 13:10:58 +0530

    update : files modified

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register'; 
 import Login from './pages/Login';
+import HomePage from './pages/Home-page/Index'
+import ProfilePage from './pages/Profile-page/Index'
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />          {/* Home route */}
         <Route path="/register" element={<Register />} /> {/* Register route */}
-        {/* Add more routes as needed */}
+        <Route path='/home' element={<HomePage/>}></Route>
+        <Route path='/profile' element={<ProfilePage/>} ></Route>       {/* Add more routes as needed */}
       </Routes>
     </BrowserRouter>
   );
